fdb79bd8a95381c3ecfc2a981b09486f3a0db629
Jordi Armengol Estapé <jordi.armengol.estape@c6s308pc33.fib.upc.es>
2016-12-16 14:03:56 +0100

    document

--- B7_R_Studio.R
+++ B7_R_Studio.R
@@ -13,41 +13,15 @@ plot(dades$N,dades$Insercio,type="l",main="Gràfic n°elements/temps per inserci
 
 plot(dades$N,dades$Bombolla,type="l",main="Gràfic n°elements/temps per bombolla",lwd=5,col="red",xlab="N", ylab="Temps")
 
-plot(dades$N,dades$Fusio,type="l",main="Gràfic n°elements/temps per fusió",lwd=5,col="orange",xlab="N", ylab="Temps")
+#plot(dades$N,dades$Fusio,type="p",main="Gràfic n°elements/temps per fusió",lwd=5,col="orange",xlab="N", ylab="Temps")
 
 #Plot dels 3 junts
-
+dades
 
 plot(dades$N, dades$Bombolla, type="l", lwd=5, col="blue",xlab="N", ylab="Temps", xaxs="i", yaxs="i")
 lines(dades$N,dades$Insercio, lwd=5, col="red")
-lines(dades$N,dades$Fusio, lwd=5, col="green")
+#lines(dades$N,dades$Fusio, lwd=20, col="green")
 legend("topleft", legend=c("Bombolla","Insercio","Fusio"), lwd=c(3,3,3), col=c("blue","red","green"))
-
-#New data(millors i pitjors casos)
-dadesBWCases = read.table("data_best_worst_cases.txt",header=TRUE)
-#7 Columnes => N Worst_insecio Worst_Bombolla Worst_Fusio Best_insecio Best_Bonmbolla Best_Fusio
-summary(dadesBWCases)
-
-plot(dadesBWCases$N, dadesBWCases$BombollaWorst, type="l", lwd=3, col="#1E0FF2",xlab="N", ylab="Temps", xaxs="i", yaxs="i")
-lines(dadesBWCases$N,dadesBWCases$BombollaBest, lwd=5, col="#00FFCC")
-
-plot(dadesBWCases$N, dadesBWCases$FusioWorst, type="l", lwd=3, col="#990000",xlab="N", ylab="Temps", xaxs="i", yaxs="i")
-lines(dadesBWCases$N,dadesBWCases$FusioBest, lwd=5, col="#FF9151")
-
-
-plot(dadesBWCases$N, dadesBWCases$InsercioWorst, type="l", lwd=3, col="#006600",xlab="N", ylab="Temps", xaxs="i", yaxs="i")
-lines(dadesBWCases$N,dadesBWCases$InsercioBest, lwd=5, col="#11DB69")
-
-
-
-plot(dadesBWCases$N, dadesBWCases$BombollaWorst, type="l", lwd=3, col="#1E0FF2",xlab="N", ylab="Temps", xaxs="i", yaxs="i")
-lines(dadesBWCases$N,dadesBWCases$BombollaBest, lwd=5, col="#00FFCC")
-lines(dadesBWCases$N,dadesBWCases$FusioWorst, lwd=5, col="#990000")
-lines(dadesBWCases$N,dadesBWCases$FusioBest, lwd=5, col="#FF9151")
-lines(dadesBWCases$N,dadesBWCases$InsercioWorst, lwd=5, col="#006600")
-lines(dadesBWCases$N,dadesBWCases$InsercioBest, lwd=5, col="#11DB69")
-legend("topleft", legend=c("Pitjor Cas Bombolla","Millor Cas Bombolla","Pitjor Cas Fusio","Millor Cas Fusio","Pitjor Cas Insercio","Millor Cas Insercio"), lwd=c(3,3,3), col=c("#1E0FF2","#00FFCC","#990000","#FF9151","#006600","#11DB69"))
-
 
 
 
@@ -72,6 +46,16 @@ mitjanaF3 = mitjanaF2*10000
 #Hipotesis nula H0: mitjanamerge = mitjanaisert 
 #               H1: mitjanamerge < mitjanainsert (unilateral)
 
+
+qqplot(dades$N,dades$Insercio)
+qqplot(dades$N,dades$Bombolla)
+
+hist(dades$Insercio)
+hist(dades$Bombolla)
+
+
+
+
 dades$IN10000<-dades$Insercio*mean(dades$N)
 dades$BN10000<-dades$Bombolla*mean(dades$N)
 dades$FN10000<-dades$Fusio*mean(dades$N) 
@@ -94,4 +78,3 @@ t.test(dades$IN10000,dades$FN10000,var.equal = TRUE)
 
 ? t.test
 #Casos extrems merge to grandes
-
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 ./clean_data.sh
-./clean_data_best_worst_Cases.sh
 ./run.sh
-./run_best_worst_cases.sh
 
